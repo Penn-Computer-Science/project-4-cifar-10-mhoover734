b7c7fcfe1d44fa1ba04d082f68f33ca2d513c89a
mhoover734 <mhoover734@phm.k12.in.us>
2025-11-07 09:51:23 -0500

    Update cifar10datasetmodel.py

--- cifar10datasetmodel.py
+++ cifar10datasetmodel.py
@@ -44,8 +44,9 @@ x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
 #Convert labels to be onehot, not sparse
-y_train = tf.one_hot(y_train.astype(np.int32), depth = 10)
-y_test = tf.one_hot(y_test.astype(np.int32), depth=10)
+from keras.utils import to_categorical
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
 
 #Show an example from MNIST
 '''plt.imshow(x_train[random.randint(0, 59999)][:,:,0], cmap='gray')
@@ -66,19 +67,22 @@ model = tf.keras.models.Sequential(
         tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
         tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu', input_shape=input_shape),
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(num_classes, activation='softmax')
     ]
 )
 
 
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(x_train, y_train,epochs=10,validation_data=(x_test, y_test))
+model.compile(optimizer='adam',loss='categorical_crossentropy', metrics=['acc'])
+history = model.fit(x_train, y_train,epochs=10,validation_split = .1)
 
 fig, ax = plt.subplots(2,1)
 ax[0].set_title("Loss")
 ax[0].set_xlabel("Epochs")
 ax[0].set_ylabel("Loss")
-
+ax[0].plot(history.history['loss'], color = 'b', label="Training Loss")
+ax[0].plot(history.history['val_loss'], color = 'r', label="Testing Loss")
 ax[1].plot(history.history['acc'], color = 'b', label="Training Accuracy")
 ax[1].plot(history.history['val_acc'], color = 'r', label="Testing Accuracy")
 legend=ax[1].legend(loc='best', shadow=True)
